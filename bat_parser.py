202873ace2d6967895f5edd81dc93f9de94dba5d
Junggu Lee <hyde1004@gmail.com>
2016-12-04 15:18:22 +0900

    initial code

---
+++ bat_parser.py
@@ -0,0 +1,11 @@
+def display_result():
+    print('* filename : stream.ts')
+    print('* BAT Info')
+    print('     Version : 0x2')
+    print('     Bouquet ID : 0x6070')
+    print('         TSID : 0x0, ONID : 0x0, Service ID : 0x0, LCN : 100')
+    print('     Bouquet ID : 0x6071')
+    print('         TSID : 0x0, ONID : 0x0, Service ID : 0x0, LCN : 100')
+
+
+display_result()
